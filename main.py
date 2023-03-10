7eb14cc11cb3bbfd51369247835cda6415ff977a
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-03-10 17:53:18 -0600

    Added main functionality

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from documentcloud.addon import AddOn, SoftTimeOutAddOn
 
-class ProjectAdder(AddOn):
+class ProjectAdder(SoftTimeOutAddOn):
     """DocumentCloud Add-On that adds a query of documents to a project"""
-
     def main(self):
-        project_id = self.data.get("project_id")
-        
+        project = self.client.projects.get(self.data.get("project_id"))
         for document in self.get_documents():
-          
+            if document not in project.document_list:
+                project.document_list.append(document)
+        project.put()
+
 if __name__ == "__main__":
     ProjectAdder().main()
