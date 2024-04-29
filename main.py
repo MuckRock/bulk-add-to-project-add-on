f714db0fd73f13327e410ccc413c8aff87ecf19c
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-04-29 14:20:21 -0500

    Try old approach

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-from documentcloud.addon import SoftTimeOutAddOn
+from documentcloud.addon import AddOn, SoftTimeOutAddOn
 from documentcloud.constants import BULK_LIMIT
 from documentcloud.toolbox import grouper
 
-class ProjectAdder(SoftTimeOutAddOn):
+class ProjectAdder(AddOn):
     """DocumentCloud Add-On that adds a query of documents to a project"""
     def main(self):
         project = self.client.projects.get(self.data.get("project_id"))
-        for doc_group in grouper(self.get_documents(), BULK_LIMIT):
-            for document in doc_group:
-                if document not in project.document_list:
-                    project.document_list.append(document)
-                    print(project.document_list)
-            project.save()
+        for document in self.get_documents():
+            if document not in project.document_list:
+                project.document_list.append(document)
+        project.save()
 
 if __name__ == "__main__":
     ProjectAdder().main()
