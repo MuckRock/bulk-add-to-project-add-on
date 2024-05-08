4613f651c6868a480190bc0a676179d700e7f2b0
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2024-05-08 14:45:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,9 @@ class ProjectAdder(AddOn):
     """DocumentCloud Add-On that adds a query of documents to a project"""
     def main(self):
         project = self.client.projects.get(self.data.get("project_id"))
-        print("project document list before adds") 
-        print(project.document_list)
         for document in self.get_documents():
             if document not in project.document_list:
-                print(f"Adding document {document.id}")
                 project.document_list.append(document)
-            else: 
-                print("Else branch reached")
         project.save()
 
 if __name__ == "__main__":
