3c4be569030151b7e23ca068c0bbee432e6053ca
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2024-05-09 18:27:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class ProjectAdder(AddOn):
                 if document is not None:
                     docs_to_add.append(document)
             self.add_to_project(project, docs_to_add)
-            time.sleep(10)
+            time.sleep(30)
 
 if __name__ == "__main__":
     ProjectAdder().main()
