c0f8307975b874a45fe2e5a4cc7637bcc15c280e
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2024-05-09 16:30:21 -0500

    Try another approach to bulk add.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from documentcloud.addon import AddOn, SoftTimeOutAddOn
 from documentcloud.constants import BULK_LIMIT
 from documentcloud.toolbox import grouper
+import time
 
 class ProjectAdder(AddOn):
     """DocumentCloud Add-On that adds a query of documents to a project"""
     def main(self):
         project = self.client.projects.get(self.data.get("project_id"))
-        for document in self.get_documents():
-            if document not in project.document_list:
-                project.document_list.append(document)
-        project.save()
+        for doc_group in grouper(self.get_documents(), BULK_LIMIT):
+            docs_to_add = [] 
+            for document in doc_group:
+                docs_to_add.append(document)
+                project.add_documents(docs_to_add)
 
 if __name__ == "__main__":
     ProjectAdder().main()
