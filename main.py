177d82ac1c08e4a34255be899ab5539197f645b7
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2024-05-09 17:14:48 -0500

    Adds exponential backoff

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from documentcloud.constants import BULK_LIMIT
 from documentcloud.toolbox import grouper
 import time
 
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)  # for exponential backoff
+
 class ProjectAdder(AddOn):
     """DocumentCloud Add-On that adds a query of documents to a project"""
+    @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+    def add_to_project(self, project, docs_to_add_to_project):
+        project.add_documents(docs_to_add_to_project)
+        
     def main(self):
         project = self.client.projects.get(self.data.get("project_id"))
         for doc_group in grouper(self.get_documents(), 100):
@@ -12,7 +22,7 @@ class ProjectAdder(AddOn):
             for document in doc_group:
                 if document is not None:
                     docs_to_add.append(document)
-            project.add_documents(docs_to_add)
+            self.add_to_project(project, docs_to_add)
             time.sleep(10)
 
 if __name__ == "__main__":
